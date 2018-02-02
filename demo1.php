e0d7b7afc8d54fb038ca138346b25209f6254271
huzhengen <huzhengen@163.com>
2018-02-02 16:58:06 +0800

    demo1 demo2

---
+++ demo1.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set('PRC');
+echo strtotime('now'),'<br>';
+echo strtotime('today'),'<hr>';
+echo ceil(3.16),'<br>';
+echo floor(9.99),'<br>';
+echo log(5),'<br>';
---
+++ demo2.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type:text/html; charset=utf-8');
+
+class Computer{
+	public $_name;
+	public $_model;
+	function _run(){
+		echo "这是运行的方法~";
+	}
+};
+$computer1 = new Computer();
+
+$computer1->_name = '联想~';
+$computer1->_model = 'i5~';
+
+var_dump($computer1);
+echo "<br>";
+echo $computer1->_name,'<br>';
+echo $computer1->_run(),'<hr>';
+
+setcookie('user', 'lufang', time()+3600);
+
+echo $_COOKIE['user'],'<br>';
+print_r($_COOKIE);
